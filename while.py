94eee75c7096bcd482ef934924462c17fa115a64
ariefjalil <up.prozac3@gmail.com>
2024-06-21 01:39:03 +0800

    first commit

--- while.py
+++ while.py
@@ -1,12 +1,12 @@
 while True:
 	print('Who are you?')
-	name = input()
+	name = input('>')
 
 	if name != 'Joe':
 		continue
 
-	print('Hello Joe. What is the password?')
-	password = input()
+	print(f"Hello {name}. What is the password?")
+	password = input('>')
 	if password == 'swordfish':
 		break
 print('Access granted!')
