6e8b0754c260c3c969b7d9365afb12e0387055c6
ariefjalil <up.prozac3@gmail.com>
2024-06-25 23:33:30 +0800

    added parameter

--- ajx_collatz.py
+++ ajx_collatz.py
@@ -1,18 +1,40 @@
 
 
-def collatz():
+# def collatz():
 
-	print('Enter number:')
-	user_input = int(input('> '))
+# 	print('Enter number:')
+# 	user_input = int(input('> '))
 
-	while user_input != 1:
-		if user_input %2 == 0:
-			user_input //=2
-
-
-		else:
-			user_input = 3 * user_input + 1
-		print(user_input)
+# 	while user_input != 1:
+# 		if user_input %2 == 0: #check if even number
+# 			user_input //=2
 
 
-collatz()
+# 		else:
+# 			user_input = 3 * user_input + 1
+# 		print(user_input)
+
+
+# collatz()
+
+#----or add parameter number in collatz function ---
+
+# def collatz(number):
+# 	print(number)
+
+# 	while number !=1:
+# 		if number % 2 == 0:
+# 			number //=2
+
+# 		else:
+# 			number = 3 * number +1
+# 		print(f'this is the {number}')
+
+
+
+
+# number = int(input('Enter number: '))
+# collatz(number)
+
+
+#----
